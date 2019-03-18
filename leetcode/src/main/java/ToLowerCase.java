c2754b86a681f189a78eccc8cdeba0f7a1f63eba
heming2018 <1023742249@qq.com>
2019-03-18 15:18:00 +0800

    ToLowerCase

---
+++ leetcode/src/main/java/ToLowerCase.java
@@ -0,0 +1,35 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author heming1
+ * @date 2019/3/18 14:52
+ * @description TODO
+ */
+public class ToLowerCase {
+    private static final Logger logger = LoggerFactory.getLogger(ToLowerCase.class);
+
+    public static void main(String[] args) {
+    }
+
+    public static String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+
+    /**
+     * 'a' - 'A' = 32
+     */
+    public static String toLowerCaseAvoidLibrary(String str) {
+        char[] chars = str.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c >= 'A' && c <= 'Z') {
+                chars[i] = (char) (c - 'A' + 'a');
+            }
+        }
+        return new String(chars);
+
+    }
+
+}
