6ebd2bbd21faabed2e6e50f25f30b87e50688764
heming2018 <1023742249@qq.com>
2019-03-22 09:56:22 +0800

    EqualsTest

---
+++ unit-test/src/main/java/EqualsTest.java
@@ -0,0 +1,15 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author heming1
+ * @date 2019/3/22 9:51
+ * @description TODO
+ */
+public class EqualsTest {
+    private static final Logger logger = LoggerFactory.getLogger(EqualsTest.class);
+
+    public static void main(String[] args) {
+        System.out.println(null instanceof Exception);
+    }
+}
