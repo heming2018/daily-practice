eea2544cfe332bfab873e594b3175f81a73602dc
heming2018 <1023742249@qq.com>
2019-11-06 14:01:10 +0800

    add

---
+++ unit-test/src/main/java/email/SendEmail.java
@@ -0,0 +1,71 @@
+package email;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * @author heming
+ * @date 2019-06-06 13:50
+ * @description TODO
+ */
+public class SendEmail {
+    private static final Logger logger = LoggerFactory.getLogger(SendEmail.class);
+
+    public static void main(String[] args) {
+        // 收件人电子邮箱
+        String to = "heming201871@icloud.com";
+
+        // 发件人电子邮箱
+        String from = "1023742249@qq.com";
+
+        // 指定发送邮件的主机为 smtp.qq.com
+        String host = "smtp.qq.com";  //QQ 邮件服务器
+
+        // 获取系统属性
+        Properties properties = System.getProperties();
+
+        // 设置邮件服务器
+        properties.setProperty("mail.smtp.host", host);
+
+        properties.put("mail.smtp.auth", "true");
+        // 获取默认session对象
+        Session session = Session.getDefaultInstance(properties, new Authenticator() {
+            @Override
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("1023742249@qq.com", "wvxjsuvtmtuybfbi"); //发件人邮件用户名、授权码
+            }
+        });
+
+        try {
+            // 创建默认的 MimeMessage 对象
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: 头部头字段
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: 头部头字段
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+
+            // Set Subject: 头部头字段
+            message.setSubject("This is the Subject Line!");
+
+            // 设置消息体
+            message.setText("This is actual message");
+
+            // 发送消息
+            Transport.send(message);
+            System.out.println("Sent message successfully....from runoob.com");
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (javax.mail.MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
