c7b9c86767f2ccc55a08dbc2bb2b7ea5475800c1
heming2018 <1023742249@qq.com>
2019-11-12 14:22:10 +0800

    输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。

---
+++ leetcode/src/main/java/NumberOf1.java
@@ -0,0 +1,15 @@
+/**
+ * @author heming
+ * @date 2019/11/12 14:13
+ * @description 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ */
+public class NumberOf1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int NumberOf1(int n) {
+        return 0;
+    }
+}
