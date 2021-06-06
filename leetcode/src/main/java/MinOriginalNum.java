a50bb279633b805f6857dcb24e3efb577d12e038
heming2018 <1023742249@qq.com>
2021-06-06 22:43:19 +0800

    优化

--- leetcode/src/main/java/MinOriginalNum.java
+++ leetcode/src/main/java/MinOriginalNum.java
@@ -4,8 +4,7 @@ import org.slf4j.LoggerFactory;
 /**
  * @author heming1
  * @date 2021/6/6 5:06 下午
- * @description
- * 1.给定一个正值整数，例 1233421
+ * @description 1.给定一个正值整数，例 1233421
  * 2.交换任意位置，不限次数，可以得到一个大于给定整数的新整数，例：3321421
  * 3.编写一个程序，输出步骤2换位后所得数字中最小的一个，例：1234123
  * 4.如果找不到，输出原值
@@ -21,8 +20,7 @@ public class MinOriginalNum {
         // 1889830122
         // 16643264321
         // 16643612234
-
-        long res = get(n);
+        long res = get(1234567);
         System.out.println(res);
     }
 
@@ -33,11 +31,9 @@ public class MinOriginalNum {
             arr[i] = s.charAt(i) - '0';
         }
 
-//        boolean exist = false;
         int rightThanLeftIndex = -1;
         for (int i = arr.length - 1; i > 0; i--) {
             if (arr[i] - arr[i - 1] > 0) {
-//                exist = true;
                 int temp = arr[i - 1];
                 arr[i - 1] = arr[i];
                 arr[i] = temp;
@@ -54,25 +50,19 @@ public class MinOriginalNum {
         }
         int end = arr.length - 1;
 
-        int[] arrNew = sort(arr, rightThanLeftIndex, end);
-        return arr2Long(arrNew);
+        sort(arr, rightThanLeftIndex, end);
+        return arr2Long(arr);
     }
 
-    private static int[] sort(int[] arr, int startIndex, int endIndex) {
-        if (startIndex > endIndex) {
-            throw new RuntimeException("endIndex must bigger than startIndex!");
-        }
-
-        for (int i = startIndex + 1; i <= endIndex; i++) {
-            int compareIndex = i - 1;
-            int currNum = arr[i];
-            while (currNum < arr[compareIndex] && compareIndex >= startIndex) {
-                arr[compareIndex+1] = arr[compareIndex];
-                arr[compareIndex] = currNum;
-                compareIndex--;
+    private static void sort(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int curr = arr[i];
+            int j = i - 1;
+            for (; j >= left && arr[j] > curr; j--) {
+                arr[j + 1] = arr[j];
             }
+            arr[j] = curr;
         }
-        return arr;
     }
 
     private static long arr2Long(int[] arr) {
