4179ef17d6e046a17fc2a023362d59b332c23a39
heming2018 <1023742249@qq.com>
2021-07-25 00:58:09 +0800

    add todo

--- algorithm-practice/src/main/java/leetcode/LC46_permute.java
+++ algorithm-practice/src/main/java/leetcode/LC46_permute.java
@@ -10,7 +10,7 @@ import java.util.Stack;
  * @description 46. 全排列
  * https://leetcode-cn.com/problems/permutations/
  * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
- * 这道题不太懂！！！
+ * TODO 这道题不太懂！！！
  * <p>
  * 输入：nums = [1,2,3]
  * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
