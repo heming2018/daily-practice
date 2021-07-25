05d88e4de418653617ad79c261a624372c9782da
heming2018 <1023742249@qq.com>
2021-07-26 01:19:43 +0800

    加注释

--- algorithm-practice/src/main/java/leetcode/LC88_merge.java
+++ algorithm-practice/src/main/java/leetcode/LC88_merge.java
@@ -4,6 +4,7 @@ package leetcode;
  * @author heming1
  * @date 2021/7/26 12:39 上午
  * @description 88. 合并两个有序数组
+ * https://leetcode-cn.com/problems/merge-sorted-array/submissions/
  * 给你两个有序整数数组nums1 和 nums2，请你将 nums2 合并到nums1中，使 nums1 成为一个有序数组。
  * <p>
  * 初始化nums1 和 nums2 的元素数量分别为m 和 n 。你可以假设nums1 的空间大小等于m + n，这样它就有足够的空间保存来自 nums2 的元素。
